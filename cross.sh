03adc4bec561b9f94bbc35dc41c16bc10f496af7
Peter Foley <pefoley2@pefoley.com>
2016-03-13 16:30:34 -0400

    more refactoring

--- cross.sh
+++ cross.sh
@@ -1,25 +1,52 @@
 #!/bin/bash
 set -e
+set -o pipefail
 DIR=`pwd`
 test -d src/gcc || git submodule add git://github.com/gcc-mirror/gcc src/gcc
 test -d src/binutils || git submodule add git://sourceware.org/git/binutils-gdb.git src/binutils
 test -d src/cygwin || git submodule add git://sourceware.org/git/newlib-cygwin.git src/cygwin
 test -n "$TARGET" || TARGET=x86_64-pc-cygwin
 test -n "$PARALLEL" || PARALLEL=$((`nproc`+1))
-mkdir -p logs work/gcc1
-#mkdir -p work/{mingw-{headers,w64},gcc_bootstrap}
+mkdir -p logs
 
 #cd work/mingw-headers
 #test -f Makefile || ($DIR/src/mingw-w64/mingw-w64-headers/configure --host=${TARGET} --prefix=${DIR}/install/${TARGET} || rm Makefile)
 #test -f $DIR/install/$TARGET/include/_mingw_mac.h || make install
 
-cd work/gcc1
-test -f Makefile || $DIR/src/gcc/configure --target=${TARGET} --prefix=${DIR}/install --enable-languages=c |& tee $DIR/logs/gcc1.log
-make -j${PARALLEL}
-exit
-make all-host all-target-{libgcc,newlib} -j${PARALLEL}
-test -f $DIR/install/bin/$TARGET-gcc || make install-host install-target-{libgcc,newlib} -j5
+conf()
+{
+    src=$1
+    shift
+    work=$1
+    shift
+    mkdir -p $DIR/work/$work
+    cd $DIR/work/$work
+    test -e Makefile || $DIR/src/$src/configure $@ |& tee $DIR/logs/$work-config.log
+}
 
-#cd ../mingw-w64
-#test -f Makefile || (PATH=$DIR/install/bin:${PATH} $DIR/src/mingw-w64/configure --host=${TARGET} --prefix=${DIR}/install/${TARGET} || rm Makefile)
-#PATH=$DIR/install/bin:$PATH make
+build()
+{
+    work=$1
+    shift
+    make -j${PARALLEL} -C $DIR/work/$work $@ |& tee $DIR/logs/$work-build.log
+}
+
+install()
+{
+    work=$1
+    shift
+    make -j${PARALLEL} -C $DIR/work/$work $@ |& tee $DIR/logs/$work-install.log
+}
+
+conf binutils binutils --target=${TARGET} --prefix=${DIR}/install
+build binutils all-{binutils,ld,gas}
+install binutils install-{binutils,ld,gas}
+
+conf gcc gcc1 --target=${TARGET} --prefix=${DIR}/install --enable-languages=c
+build gcc1 all-gcc
+exit
+install gcc1 install-gcc
+
+conf gcc gcc2 --target=${TARGET} --prefix=${DIR}/install --enable-languages=c++
+build gcc2 all
+install gcc2 install
