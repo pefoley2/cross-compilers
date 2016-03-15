7c75b4491437dd701525f5198673ec772456745d
Peter Foley <pefoley2@pefoley.com>
2016-03-15 14:12:26 -0400

    cleanup

--- cygwin.sh
+++ cygwin.sh
@@ -8,7 +8,6 @@ test -d src/gcc       || git submodule add git://github.com/gcc-mirror/gcc src/g
 test -d src/binutils  || git submodule add git://sourceware.org/git/binutils-gdb.git src/binutils
 test -d src/cygwin    || git submodule add git://sourceware.org/git/newlib-cygwin.git src/cygwin
 test -d src/mingw     || git submodule add https://github.com/mirror/mingw-w64 src/mingw
-#test -d src/zlib      || (wget -q http://zlib.net/zlib-1.2.8.tar.gz -O - | tar xz -C src; mv src/zlib-1.2.8 src/zlib)
 mkdir -p logs
 
 test -n "$PARALLEL" || PARALLEL=$((`nproc`+1))
@@ -44,36 +43,6 @@ install()
 }
 
 # no deps
-#conf mingw/mingw-w64-headers mingw-headers-native --target=${MINGW} --prefix=${DIR}/install/${MINGW}
-#build mingw-headers-native all
-#test -e $DIR/install/${MINGW}/include/windows.h || install mingw-headers-native install
-
-# no deps
-#conf binutils mingw-binutils --target=${MINGW} --prefix=${DIR}/install
-#build mingw-binutils all-{binutils,ld,gas}
-#test -e ${MINGW_PREFIX}-ld || install mingw-binutils install-{binutils,ld,gas}
-
-# need mingw-binutils
-#conf gcc mingw-gcc --target=${MINGW} --prefix=${DIR}/install --enable-languages=c++ --disable-multilib
-#build mingw-gcc all-gcc
-#test -e ${MINGW_PREFIX}-gcc || install mingw-gcc install-gcc
-
-# need mingw-gcc
-#cd $DIR/src/zlib
-#test -e libz.a || CROSS_PREFIX=$DIR/install/bin/${MINGW}- ./configure --prefix=$DIR/install/${MINGW}
-#test -e $DIR/install/${MINGW}/lib/libz.a || make -j${PARALLEL} -C $DIR/src/zlib install
-
-# need mingw-gcc
-#conf mingw/mingw-w64-crt mingw-crt-native --host=${MINGW} --prefix=${DIR}/install/${MINGW} --disable-lib32 \
-#    CC=${MINGW_PREFIX}-gcc DLLTOOL=${MINGW_PREFIX}-dlltool AS=${MINGW_PREFIX}-as AR=${MINGW_PREFIX}-ar RANLIB=${MINGW_PREFIX}-ranlib
-#build mingw-crt-native all
-#test -e $DIR/install/${MINGW}/lib/libkernel32.a || install mingw-crt-native install
-
-# need mingw-crt-native
-#build mingw-gcc all-target-libstdc++-v3
-#test -e ${DIR}/install/${MINGW}/lib/libstdc++-6.dll || install mingw-gcc install-target-libstdc++-v3
-
-# no deps
 conf mingw/mingw-w64-headers mingw-headers --target=${TARGET} --prefix=${DIR}/install/${TARGET} --enable-w32api
 build mingw-headers all
 test -e $DIR/install/${TARGET}/include/w32api/windows.h || install mingw-headers install
@@ -95,12 +64,7 @@ conf cygwin cygwin1 --target=${TARGET} --prefix=${DIR}/install --with-build-time
 build cygwin1 all-target-newlib
 test -e $DIR/install/${TARGET}/lib/libc.a || install cygwin1 install-target-newlib
 
-# FIXME: shouldn't need this.
-#mkdir -p ${DIR}/install/${TARGET}/lib/w32api
-#test -e ${DIR}/install/${TARGET}/lib/w32api/libntdll.a || ar r ${DIR}/install/${TARGET}/lib/w32api/libntdll.a
-
 # need gcc1
-#conf cygwin/winsup/cygwin cygwin-headers --host=${TARGET} --prefix=${DIR}/install/${TARGET} CC=${TARGET_PREFIX}-gcc
 # FIXME: objcopy for target
 build cygwin1 configure-target-winsup OBJCOPY=${TARGET_PREFIX}-objcopy
 # FIXME: winsup-level target
@@ -113,20 +77,11 @@ conf mingw/mingw-w64-crt mingw-crt --host=${TARGET} --prefix=${DIR}/install/${TA
 build mingw-crt all
 test -e $DIR/install/${TARGET}/lib/w32api/libkernel32.a || install mingw-crt install
 
-# FIXME: shouldn't need this.
-#test -e ${DIR}/install/${TARGET}/lib/libcygwin.a || ar r ${DIR}/install/${TARGET}/lib/libcygwin.a
-#test -e ${DIR}/install/${TARGET}/lib/crt0.o || ${TARGET_PREFIX}-gcc -xc -c - -o ${DIR}/install/${TARGET}/lib/crt0.o << EOF
-#  void __main(void) {}
-#  int atexit(void (*function)(void)) {}
-#EOF
-
-# needs cygwin-headers
+# need mingw-crt
 build gcc1 all-target-libstdc++-v3
 test -e $DIR/install/${TARGET}/lib/libstdc++.a || install gcc1 install-target-libstdc++-v3
 
 # need libstdc++-v3
-# need mingw-crt
-#MINGW64_CC=${DIR}/install/${TARGET}/bin/${MINGW}-gcc MINGW_CXX=${DIR}/install/${TARGET}/bin/${MINGW}-g++
 build cygwin1 all
 test -e $DIR/install/${TARGET}/lib/cygwin1.dll || install cygwin1 install
 
@@ -140,5 +95,4 @@ test -e $DIR/install/${TARGET}/lib/cyggcc_s-seh-1.dll || install gcc2 install
 conf cygwin cygwin2 --target=${TARGET} --prefix=${DIR}/install --with-build-time-tools=${DIR}/install/${TARGET}/bin \
     CC_FOR_TARGET=${TARGET_PREFIX}-gcc CXX_FOR_TARGET=${TARGET_PREFIX}-g++ WINDRES_FOR_TARGET=${TARGET_PREFIX}-windres
 build cygwin2 all OBJCOPY=${TARGET_PREFIX}-objcopy
-#MINGW64_CC=${DIR}/install/bin/${MINGW}-gcc MINGW_CXX=${DIR}/install/bin/${MINGW}-g++
 test -e $DIR/install/bin/cyglsa64.dll || install cygwin2 install
